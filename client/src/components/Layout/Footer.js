402d459ac40ee2edd2ad9e2b13db9516f4c14cfc
marginnasit333 <marginnasit666@gmail.com>
2023-06-24 18:48:21 +0530

    FInal

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h6 className="footer-text">All rights reserved &copy; Morgan333</h6>
+    </div>
+  );
+};
+
+export default Footer;
